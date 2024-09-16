1dc99eae6bb573b5476964c8ddc2f3ceac0b8d66
RalivDev <ralivdev@gmail.com>
2024-09-16 17:02:44 -0500

    groundwork

--- Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidRenderingFRendererFeature.cs
+++ Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidRenderingFRendererFeature.cs
@@ -1,47 +1,84 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-public class FluidRenderingRendererFeature : ScriptableRendererFeature
-{
-    class LensFlarePass : ScriptableRenderPass
-    {
-        private Material _material;
+public class FluidRenderingRendererFeature : ScriptableRendererFeature {
+    
+    class FluidPass : ScriptableRenderPass {
+        private Material material;
+        private Material material2;
         private Mesh _mesh;
-
-        public LensFlarePass(Material material, Mesh mesh)
-        {
-            _material = material;
-            _mesh = mesh;
+        private static readonly ShaderTagId ShaderTagID = new ShaderTagId(nameof(FluidPass));
+        private static readonly int tempRTPropertyID = Shader.PropertyToID("_FluidRT");
+        private static readonly int grabTexturePropertyID = Shader.PropertyToID("_MyGrabTexture");
+        
+        private RenderTextureDescriptor textureDescriptor;
+        private RTHandle textureHandle;
+        
+        public FluidPass(Material material, Material material2) {
+            this.material = material;
+            this.material2 = material2;
+            renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+            textureDescriptor = new RenderTextureDescriptor(Screen.width,
+                Screen.height, RenderTextureFormat.Default, 0);
         }
 
-        public override void Execute(ScriptableRenderContext context,
-            ref RenderingData renderingData)
-        {
-            CommandBuffer cmd = CommandBufferPool.Get(name: "LensFlarePass");
-            cmd.DrawMesh(_mesh, Matrix4x4.identity, _material);
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor) {
+            textureDescriptor.width = cameraTextureDescriptor.width;
+            textureDescriptor.height = cameraTextureDescriptor.height;
+            RenderingUtils.ReAllocateIfNeeded(ref textureHandle, textureDescriptor);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
+            var cmd = CommandBufferPool.Get();
+            RTHandle cameraTargetHandle = renderingData.cameraData.renderer.cameraColorTargetHandle;
+            Blit(cmd, cameraTargetHandle, textureHandle, material);
+            Blit(cmd, textureHandle, cameraTargetHandle, material);
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
+
+            //var drawingSettings =
+            //    CreateDrawingSettings(ShaderTagID, ref renderingData, SortingCriteria.CommonTransparent);
+            //var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+            //context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings);
+        }
+
+        private void Dispose() {
+            
+#if UNITY_EDITOR
+            if (EditorApplication.isPlaying) {
+                Destroy(material);
+            } else {
+                DestroyImmediate(material);
+            }
+#else
+            Object.Destroy(material);
+#endif
+
+            if (textureHandle != null) textureHandle.Release();
+            
         }
     }
 
-    private LensFlarePass _lensFlarePass;
-    public Material material;
-    public Mesh mesh;
+    [SerializeField] private Material material;
+    [SerializeField] private Material material2;
+    private FluidPass _fluidPass;
 
-    public override void Create()
-    {
-        _lensFlarePass = new LensFlarePass(material, mesh);
+    public override void Create() {
+        _fluidPass = new FluidPass(material, material2);
+        _fluidPass.renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
     }
 
-    public override void AddRenderPasses(ScriptableRenderer renderer,
-        ref RenderingData renderingData)
-    {
-        if (material != null && mesh != null)
-        {
-            renderer.EnqueuePass(_lensFlarePass);
-        }
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+        renderer.EnqueuePass(_fluidPass);
     }
+
+    protected override void Dispose(bool disposing) {
+        
+        //CoreUtils.Destroy(material);
+    }
+    
 }
