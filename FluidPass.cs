34b6f76992dd188030ffebf8610f10d70a21a1b5
naelstrof <naelstrof@gmail.com>
2024-09-17 15:20:18 -0600

    blegh

--- FluidPass.cs
+++ FluidPass.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.RendererUtils;
 using UnityEngine.Rendering.Universal;
 
 public class FluidPass : ScriptableRenderPass {
     private Material material;
+    private LayerMask vfxMask;
     
     private ProfilingSampler m_ProfilingSampler = new ProfilingSampler("FluidRendering_RenderFeature");
-    //private const string k_OutputName = "_FluidRT";
-    //private int m_OutputId = Shader.PropertyToID(k_OutputName);
     
     private RTHandle m_CameraColorTarget;
-    private RTHandle m_CopiedColor;
+    private RTHandle m_FluidBuffer;
     
-    public FluidPass(RenderPassEvent renderPassEvent, Material material) {
+    public FluidPass(RenderPassEvent renderPassEvent, Material material, LayerMask vfxMask) {
         this.material = material;
         this.renderPassEvent = renderPassEvent;
+        this.vfxMask = vfxMask;
     }
 
     public void SetTarget(RTHandle colorHandle) {
@@ -26,14 +27,15 @@ public class FluidPass : ScriptableRenderPass {
         ReAllocate(renderingData.cameraData.cameraTargetDescriptor);
     }
 
-    private static void ExecuteCopyColorPass(CommandBuffer cmd, RTHandle sourceTexture) {
-        Blitter.BlitTexture(cmd, sourceTexture, new Vector4(1, 1, 0, 0), 0.0f, false);
-    }
+    //private static void ExecuteCopyColorPass(CommandBuffer cmd, RTHandle sourceTexture) {
+        //Blitter.BlitTexture(cmd, sourceTexture, new Vector4(1, 1, 0, 0), 0.0f, false);
+    //}
     
     void ReAllocate(RenderTextureDescriptor desc) {
         desc.msaaSamples = 1;
-        desc.depthBufferBits = (int)DepthBits.None;
-        RenderingUtils.ReAllocateIfNeeded(ref m_CopiedColor, desc, name: "_FullscreenPassColorCopy");
+        desc.depthBufferBits = (int)DepthBits.Depth32;
+        //desc.colorFormat = RenderTextureFormat.Default;
+        RenderingUtils.ReAllocateIfNeeded(ref m_FluidBuffer, desc, name: "_FluidBuffer");
     }
 
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData) {
@@ -47,11 +49,27 @@ public class FluidPass : ScriptableRenderPass {
         
         CommandBuffer cmd = CommandBufferPool.Get();
         using (new ProfilingScope(cmd, m_ProfilingSampler)) {
-            CoreUtils.SetRenderTarget(cmd, m_CopiedColor);
-            ExecuteCopyColorPass(cmd, cameraData.renderer.cameraColorTargetHandle);
-            
-            CoreUtils.SetRenderTarget(cmd, cameraData.renderer.cameraColorTargetHandle);
-            Blitter.BlitCameraTexture(cmd, m_CopiedColor, m_CameraColorTarget, material, 0);
+            CoreUtils.SetRenderTarget(cmd, m_FluidBuffer);
+            CoreUtils.ClearRenderTarget(cmd, ClearFlag.All, Color.black);
+            if (cameraData.camera.TryGetCullingParameters(out var cullingParameters)) {
+                //cullingParameters.cullingMask = (uint)vfxMask.value;
+                var shaderTags = new ShaderTagId[] {
+                    new ShaderTagId("DepthOnly"),
+                    new ShaderTagId("UniversalForward"),
+                    new ShaderTagId("UniversalForwardOnly"),
+                    new ShaderTagId("LightweightForward"),
+                    new ShaderTagId("SRPDefaultUnlit"),
+                    new ShaderTagId("Forward")
+                };
+                //ShaderTagId shaderTagID = new ShaderTagId("UniversalForward");
+                var cullingResults = context.Cull(ref cullingParameters);
+                var desc = new RendererListDesc(shaderTags, cullingResults, cameraData.camera) {
+                    renderQueueRange = RenderQueueRange.all,
+                };
+                var rendererList = context.CreateRendererList(desc);
+                cmd.DrawRendererList(rendererList);
+            }
+            Blitter.BlitCameraTexture(cmd, m_FluidBuffer, m_CameraColorTarget, material, 0);
         }
         context.ExecuteCommandBuffer(cmd);
         cmd.Clear();
