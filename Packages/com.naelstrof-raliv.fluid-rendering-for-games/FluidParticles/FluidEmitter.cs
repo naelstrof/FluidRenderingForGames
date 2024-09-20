ebcd73834e7fd9c32046d047aa67c55a135caee6
naelstrof <naelstrof@gmail.com>
2024-09-19 20:24:03 -0600

    Okay, rendering in editor kinda, also included decal tech

--- Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidParticles/FluidEmitter.cs
+++ Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidParticles/FluidEmitter.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class FluidEmitter : MonoBehaviour {
 
     [SerializeField] private Material particleMaterial;
@@ -12,20 +16,58 @@ public class FluidEmitter : MonoBehaviour {
     private Vector3 _previousPosition;
     private Vector3 _previousForward;
     private float _previousStrength;
+    private bool rendered = false;
+    private SceneView targetSceneView;
 
     private void OnEnable() {
+#if UNITY_EDITOR
+        EditorApplication.pauseStateChanged += OnPauseChanged;
+#endif
         _fluidParticleSystem = new FluidParticleSystem(particleMaterial);
     }
 
+#if UNITY_EDITOR
+    private void OnPauseChanged(PauseState obj) {
+        if (obj == PauseState.Paused) {
+            rendered = false;
+            SceneView.duringSceneGui += OnSceneGUI;
+        } else {
+            SceneView.duringSceneGui -= OnSceneGUI;
+        }
+    }
+
+    private void OnSceneGUI(SceneView obj) {
+        switch (Event.current.type) {
+            case EventType.Repaint:
+                if (SceneView.currentDrawingSceneView == null || !SceneView.currentDrawingSceneView.hasFocus) {
+                    rendered = false;
+                    return;
+                }
+                if (!rendered) {
+                    _fluidParticleSystem?.Render();
+                    rendered = true;
+                }
+                break;
+        }
+    }
+#endif
+
     private void OnDisable() {
+#if UNITY_EDITOR
+        EditorApplication.pauseStateChanged += OnPauseChanged;
+        SceneView.duringSceneGui -= OnSceneGUI;
+#endif
         _fluidParticleSystem.Cleanup();
     }
 
     private void Update() {
-        _fluidParticleSystem.Render();
+        _fluidParticleSystem?.Render();
     }
 
     private void FixedUpdate() {
+        if (!Application.isPlaying) {
+            return;
+        }
         int subParticles = 1 + (int)(_strength * 8);
         for (int i = 0; i < subParticles; i++) {
             _fluidParticleSystem.SpawnParticle(
