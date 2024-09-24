1b31c64ac8c4c9a18499b498b353f0eb6ba2ba5a
naelstrof <naelstrof@gmail.com>
2024-09-23 19:35:07 -0600

    Strand polish

--- Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidParticles/FluidParticleSystemVerletStrand.cs
+++ Packages/com.naelstrof-raliv.fluid-rendering-for-games/FluidParticles/FluidParticleSystemVerletStrand.cs
@@ -11,14 +11,18 @@ public class FluidParticleSystemVerletStrand : FluidParticleSystem {
 
     private bool broken;
     private float timeBroken;
-    public const float fadeoutTime = 1f;
+    public const float fadeoutTime = 0.4f;
     public bool GetBroken() => broken;
     public float GetTimeBroken() => timeBroken;
 
     private const float baseSize = 1f;
-    private const float baseHeightStrength = 4f;
-    private const float cubicHeightStrength = 25f;
-    private const float breakForce = 1.2f;
+    private const float baseHeightStrength = 1.75f;
+    private const float cubicHeightStrength = 5f;
+    private const float breakDistance = .5f;
+    
+    const float constraintStrength = 0.25f; // Range 0f to 0.5f
+    const float friction = 0.08f; // Range 0f to 1f
+    const float gravityMult = 1f;
     
     
     private FluidParticleSystemSettings settings;
@@ -48,8 +52,6 @@ public class FluidParticleSystemVerletStrand : FluidParticleSystem {
                 _particles[i].heightStrength = (baseHeightStrength + (easing*cubicHeightStrength)) * fluidParticleSystemSettings.heightStrengthBase;
             }
         }
-
-        _particles[0].heightStrength = 2f;
     }
 
     public void SetLocalPointA(Vector3 point) {
@@ -58,37 +60,41 @@ public class FluidParticleSystemVerletStrand : FluidParticleSystem {
     public Vector3 ExpDecay(Vector3 a, Vector3 b, float decay, float dt) {
         return b + (a - b) * Mathf.Exp(-decay * dt);
     }
-
-    private bool CheckBreak(float mag) {
-        if (!broken && mag > breakForce / _particles.Length) {
-            broken = true;
-            timeBroken = Time.time;
-        }
-        return broken;
-    }
     protected override void UpdateParticles() {
-        const float constraintStrength = 0.25f; // Range 0f to 0.5f
-        const float friction = 0.06f; // Range 0f to 1f
-        const float gravityMult = 1f;
         float realFriction = 1f - (friction * friction);
+        Vector3 pointA = a.TransformPoint(localPointA);
+        Vector3 pointB = b.TransformPoint(localPointB);
         if (broken) {
+            Vector3 normal = Vector3.Normalize(pointB - pointA);
             for (var i = 0; i < _particles.Length; i++) {
                 float fadeProgress = 1f-(Time.time - timeBroken)/fadeoutTime;
-                float progress = (float)i / _particles.Length;
+                float progress = (float)i / (_particles.Length-1);
                 if (progress < 0.5f) {
-                    _particles[i].size = baseSize * settings.particleBaseSize * fadeProgress;
+                    if (Vector3.Dot(_particles[i].position-pointA, normal) < 0f) {
+                        _particles[i].size = 0f;
+                    } else if (_particles[i].size != 0f) {
+                        _particles[i].size = baseSize * settings.particleBaseSize * fadeProgress;
+                    }
                 } else {
-                    _particles[i].size = baseSize * settings.particleBaseSize * fadeProgress;
+                    if (Vector3.Dot(pointB-_particles[i].position, normal) < 0f) {
+                        _particles[i].size = 0f;
+                    } else if (_particles[i].size != 0f) {
+                        _particles[i].size = baseSize * settings.particleBaseSize * fadeProgress;
+                    }
                 }
             }
         }
         _particles[0].position = a.TransformPoint(localPointA);
         _particles[^1].position = b.TransformPoint(localPointB);
+        if (!broken && Vector3.Distance(pointA, pointB) > breakDistance) {
+            broken = true;
+            timeBroken = Time.time;
+        }
         for (int iterations = 0; iterations < 10; iterations++) {
             for (var i = 1; i < _particles.Length - 1; i++) {
                 var diff = _particles[i - 1].position - _particles[i].position;
                 if (i==_particles.Length/2 || i == _particles.Length/2+1) {
-                    if (CheckBreak(diff.magnitude)) {
+                    if (broken) {
                         continue;
                     }
                 }
@@ -102,7 +108,7 @@ public class FluidParticleSystemVerletStrand : FluidParticleSystem {
             for (var i = _particles.Length-2; i >= 1; i--) {
                 var diff = _particles[i + 1].position - _particles[i].position;
                 if (i==_particles.Length/2 || i == _particles.Length/2+1) {
-                    if (CheckBreak(diff.magnitude)) {
+                    if (broken) {
                         continue;
                     }
                 }
